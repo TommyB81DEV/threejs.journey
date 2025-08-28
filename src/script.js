2389cde54992565a263da53be2a79d41899ca4fb
Tommaso Bissoli <tombisso@publicisgroupe.net>
2025-08-28 09:27:02 +0200

    Lesson 4 |FINAL

--- src/script.js
+++ src/script.js
@@ -1,42 +1,47 @@
 import * as THREE from 'three'
 
-// Canvas
+// INIT
 const canvas = document.querySelector('canvas.webgl')
-
-// Scene
 const scene = new THREE.Scene()
 
-/**
- * Object
- */
-const geometry = new THREE.BoxGeometry(1, 1, 1)
-const material = new THREE.MeshBasicMaterial({
-  color: 0xff0000,
-  // wireframe: true
-})
-const mesh = new THREE.Mesh(geometry, material)
-scene.add(mesh)
+// Constants
+const sizes = { height: 600, width: 800 }
 
-/**
- * Sizes
- */
-const sizes = {
-  width: 800,
-  height: 600,
-}
+// Objects
+const cube1 = new THREE.Mesh(
+  new THREE.BoxGeometry(1, 1, 1),
+  new THREE.MeshBasicMaterial({ color: 0xff0000 })
+)
+const cube2 = new THREE.Mesh(
+  new THREE.BoxGeometry(1, 1, 1),
+  new THREE.MeshBasicMaterial({ color: 0x3bcb03ff })
+)
 
-/**
- * Camera
- */
+// Groups
+const cubes = new THREE.Group()
+cubes.add(cube1)
+cubes.add(cube2)
+
+// Camera
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height)
-camera.position.z = 3
-scene.add(camera)
+camera.position.z = 4
 
-/**
- * Renderer
- */
-const renderer = new THREE.WebGLRenderer({
-  canvas: canvas,
-})
+// Helpers
+const axesHelper = new THREE.AxesHelper()
+
+// Tranformations
+cube1.position.set( -1 , 1 , 1 )
+cube1.rotation.set( -1 , 0 , 0 )
+cube2.position.set(1.5, 0.5, 1)
+cube2.rotation.set(-1, 0, 0)
+cubes.rotation.x = -1
+
+// Scene
+scene.add(axesHelper)
+scene.add(camera)
+scene.add(cubes)
+
+// Render
+const renderer = new THREE.WebGLRenderer({ canvas: canvas })
 renderer.setSize(sizes.width, sizes.height)
-renderer.render(scene, camera)
+renderer.render(scene,camera)
