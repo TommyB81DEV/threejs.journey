ca03abdce6cad1a39721ecbbb95380cdc392e029
Tommaso Bissoli <tombisso@publicisgroupe.net>
2025-08-29 15:10:53 +0200

    Lesson 7 | FINAL

--- src/script.js
+++ src/script.js
@@ -8,12 +8,20 @@ const scene = new THREE.Scene()
 // Constants
 const animation = true
 const cursor = { x: 0, y: 0 }
-const customCameraControl = true
-const sizes = { height: 600, width: 800 }
-const wireframe = true
+const customCameraControl = false
+const wireframe = false
+
+// API
+function aspectRatio(input){
+  return (input.width / input.height)
+}
+function fit(){
+  return { height: window.innerHeight, width: window.innerWidth }
+}
 
 // Vars
 let controls = null
+let sizes = fit()
 
 // Objects
 const cube = (new THREE.Mesh(
@@ -22,11 +30,19 @@ const cube = (new THREE.Mesh(
 ))
 
 // Camera
-const camera = new THREE.PerspectiveCamera(45, sizes.width / sizes.height , 0.1 , 100)
+const camera = new THREE.PerspectiveCamera(45, aspectRatio(sizes) , 0.1 , 100)
       camera.position.x = 2
       camera.position.y = 2
       camera.position.z = 2
       camera.lookAt(cube.position)
+
+// Render
+const renderer = (new THREE.WebGLRenderer({ canvas }))
+      renderer.setSize(sizes.width,sizes.height)
+      renderer.setPixelRatio(Math.min(window.devicePixelRatio,2))
+
+// Helpers
+const axesHelper = new THREE.AxesHelper()
 
 // Controls
 if (!customCameraControl) {
@@ -34,20 +50,35 @@ if (!customCameraControl) {
   controls.enableDamping = true
 }
 
-// Helpers
-const axesHelper = new THREE.AxesHelper()
-
-// Render
-const renderer = (new THREE.WebGLRenderer({ canvas }))
-      renderer.setSize(sizes.width, sizes.height)
-
 // Scene
 scene.add(axesHelper)
 scene.add(camera)
 scene.add(cube)
 
+// Resizing
+window.addEventListener('resize', e => {
+
+  sizes = fit()
+
+  camera.aspect = aspectRatio(sizes)
+  camera.updateProjectionMatrix()
+
+  renderer.setSize(sizes.width, sizes.height)
+
+})
+
+// Fullscreen
+window.addEventListener('dblclick',e => {
+  if (document.fullscreenElement) {
+    document.exitFullscreen()
+  }
+  else {
+    canvas.requestFullscreen()
+  }
+})
+
 // Cursor
-window.addEventListener("mousemove", e => {
+window.addEventListener('mousemove', e => {
   cursor.x = ( e.clientX / sizes.width ) - 0.5
   cursor.y = -( e.clientY / sizes.height - 0.5)
 })
@@ -65,6 +96,7 @@ function animate() {
     else controls.update()
 
     renderer.render(scene, camera)
+
     if (animation) window.requestAnimationFrame(animate)
 
 } animate()
