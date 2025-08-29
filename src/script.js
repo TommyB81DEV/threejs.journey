92f09496b0d8995b244ae7ead56d1a977e32a096
Tommaso Bissoli <tombisso@publicisgroupe.net>
2025-08-29 10:24:32 +0200

    Lesson 6 | FINAL

--- src/script.js
+++ src/script.js
@@ -1,49 +1,70 @@
 import * as THREE from 'three'
-import gsap from 'gsap'
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 
 // INIT
 const canvas = document.querySelector('canvas.webgl')
 const scene = new THREE.Scene()
 
 // Constants
+const animation = true
+const cursor = { x: 0, y: 0 }
+const customCameraControl = true
 const sizes = { height: 600, width: 800 }
+const wireframe = true
+
+// Vars
+let controls = null
 
 // Objects
-const mesh = new THREE.Mesh(
+const cube = (new THREE.Mesh(
   new THREE.BoxGeometry(1, 1, 1),
-  new THREE.MeshBasicMaterial({ color: 0x3bcb03ff })
-)
+  new THREE.MeshBasicMaterial({ color: 0x3bcb03ff, wireframe })
+))
 
 // Camera
-const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height)
-      camera.position.set(2,2,2)
-      camera.lookAt(mesh.position)
+const camera = new THREE.PerspectiveCamera(45, sizes.width / sizes.height , 0.1 , 100)
+      camera.position.x = 2
+      camera.position.y = 2
+      camera.position.z = 2
+      camera.lookAt(cube.position)
+
+// Controls
+if (!customCameraControl) {
+  controls = new OrbitControls( camera , canvas )
+  controls.enableDamping = true
+}
 
 // Helpers
 const axesHelper = new THREE.AxesHelper()
 
+// Render
+const renderer = (new THREE.WebGLRenderer({ canvas }))
+      renderer.setSize(sizes.width, sizes.height)
+
 // Scene
 scene.add(axesHelper)
 scene.add(camera)
-scene.add(mesh)
+scene.add(cube)
 
-// Render
-const renderer = new THREE.WebGLRenderer({ canvas })
-      renderer.setSize(sizes.width, sizes.height)
-
-// Time
-const clock = new THREE.Clock()
+// Cursor
+window.addEventListener("mousemove", e => {
+  cursor.x = ( e.clientX / sizes.width ) - 0.5
+  cursor.y = -( e.clientY / sizes.height - 0.5)
+})
 
 // Animate
 function animate() {
 
-    // Time
-    const elapsedTime = clock.getElapsedTime()
+    if (customCameraControl) {
+      camera.position.x = Math.sin(cursor.x * Math.PI * 2) * 5
+      camera.position.z = Math.cos(cursor.x * Math.PI * 2) * 5
+      camera.position.y = cursor.y * 5
+      camera.lookAt(cube.position)
+    }
 
-    // Update objects
-    mesh.rotation.y = elapsedTime
+    else controls.update()
 
     renderer.render(scene, camera)
-    window.requestAnimationFrame(animate)
+    if (animation) window.requestAnimationFrame(animate)
 
 } animate()
