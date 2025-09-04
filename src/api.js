8af841dc373f551ab63b9e4f6e032a26f87409f6
Tommaso Bissoli <tombisso@publicisgroupe.net>
2025-09-04 16:44:16 +0200

    Lesson 12 | WIP

--- src/api.js
+++ src/api.js
@@ -1,19 +1,20 @@
 import * as THREE from 'three'
 
+import { FontLoader } from 'three/examples/jsm/loaders/FontLoader.js'
+import { TextGeometry } from 'three/examples/jsm/geometries/TextGeometry.js'
+
 const ambientLight = new THREE.AmbientLight(0xffffff, 0.2)
 const pointLight = new THREE.PointLight(0xffffff, 50)
 
-let helpers = {}
-
+// SYNC
 export function aspectRatio(input) {
   return input.width / input.height
 }
-export function axesHelper(scene,enable){
+export function axesHelper({enable,helpers,scene}) {
   if (enable) {
     helpers.axesHelper = new THREE.AxesHelper(5)
     scene.add(helpers.axesHelper)
-  }
-  else {
+  } else {
     scene.remove(helpers.axesHelper)
   }
 }
@@ -22,11 +23,16 @@ export function fit() {
 }
 export function rotation(params) {
 
-  const { elapsedTime, meshes, speed } = params
+  const {
+    active,
+    elapsedTime,
+    meshes,
+    speed,
+  } = params
 
   for (const mesh of meshes) {
-    mesh.rotation.x = -speed * elapsedTime
-    mesh.rotation.y = speed * elapsedTime
+    mesh.rotation.x = active ? - speed * elapsedTime : 0
+    mesh.rotation.y = active ? speed * elapsedTime : 0
   }
 
 }
@@ -49,6 +55,51 @@ export function toggleCustomLights(scene, enable) {
   else {
     scene.remove(ambientLight)
     scene.remove(pointLight)
+  }
+
+}
+
+// ASYNC
+export async function createText(params) {
+
+  const {
+    color,
+    path,
+    text,
+    settings
+  } = params
+
+  try {
+
+    const fontLoader = new FontLoader()
+
+    return new Promise((resolve, reject) => {
+      fontLoader.load(
+        path,
+        font => {
+
+          const geometry = new TextGeometry(text,settings(font))
+          const material = new THREE.MeshBasicMaterial({color})
+
+          geometry.center()
+
+          const textMesh = new THREE.Mesh(geometry, material)
+
+          resolve(textMesh)
+
+        },
+        undefined,
+        error => {
+          console.error('Errore nel caricamento del font:',error)
+          reject(error)
+        }
+      )
+    })
+
+  }
+  catch (error) {
+    console.error('Errore nella creazione del testo:',error)
+    throw error
   }
 
 }
--- src/script.js
+++ src/script.js
@@ -1,30 +1,24 @@
 import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 import GUI from 'lil-gui'
-import { RGBELoader } from 'three/examples/jsm/loaders/RGBELoader.js'
 
 import {
   aspectRatio,
   axesHelper,
+  createText,
   fit,
   rotation,
-  toggleCustomLights,
 } from './api'
-import { shininess } from 'three/tsl'
 
 /* INIT */
 const canvas = document.querySelector('canvas.webgl')
+const helpers = {}
 const scene = new THREE.Scene()
 
 /* SETTINGS | Default */
 const defaultObject = {
-
   // Commons
   color: '#cc17d9',
-
-  // Cube
-  cubeColor: '#cc17d9',
-  cubeWidthSegments: 2,
 
   // Light | Point
   pointLightDistance: 5,
@@ -32,19 +26,33 @@ const defaultObject = {
   // Animation
   rotationSpeed: 0.1,
 
+  // Fonts
+  font: {
+    color: 0xffffff,
+    path: '/fonts/helvetiker_regular.typeface.json',
+    settings: font => ({
+      bevelEnabled: true,
+      bevelOffset: 0,
+      bevelSegments: 5,
+      bevelSize: 0.02,
+      bevelThickness: 0.03,
+      curveSegments: 6,
+      font,
+      depth: 0.2,
+      size: 0.5,
+    }),
+    text: 'Voice In The Desert',
+  },
+
 }
 
 /* SETTINGS | Debug */
 const debugObject = {
-  axesHelper: false,
-  color: defaultObject.cubeColor,
+  axesHelper: true,
+  color: defaultObject.color,
   customLights: true,
-  widthSegments: defaultObject.cubeWidthSegments,
-  subdivision: 2,
+  rotationEnabled: false,
 }
-
-/* CONSTANTS */
-const cursor = { x: 0, y: 0 }
 
 /* VARS */
 let sizes = fit()
@@ -52,113 +60,8 @@ let sizes = fit()
 /* CLOCK */
 const clock = new THREE.Clock()
 
-/* ENVIRONMENT */
-const rgbeLoader = (()=>{
-
-  const rgbeLoader = new RGBELoader()
-        rgbeLoader.load('./textures/environmentMap/2k.hdr', envMap => {
-          envMap.mapping = THREE.EquirectangularReflectionMapping
-          scene.background = envMap
-          scene.environment = envMap
-        })
-
-  return rgbeLoader
-
-})()
-
-/* TEXTURES */
-const textures = (()=>{
-  
-  const loadingManager = new THREE.LoadingManager()
-  const textureLoader = new THREE.TextureLoader(loadingManager)
-
-  const textures = {}
-
-  textures.textureDoorAlpha = textureLoader.load('/textures/door/alpha.jpg')
-  textures.textureDoorAmbientOcclusion = textureLoader.load('/textures/door/ambientOcclusion.jpg')
-  textures.textureDoorColor = textureLoader.load('/textures/door/color.jpg')
-  textures.textureDoorColor.colorSpace = THREE.SRGBColorSpace
-  textures.textureDoorHeight = textureLoader.load('/textures/door/height.jpg')
-  textures.textureDoorMetalness = textureLoader.load('/textures/door/metalness.jpg')
-  textures.textureDoorNormal = textureLoader.load('/textures/door/normal.jpg')
-  textures.textureDoorRoughness = textureLoader.load('/textures/door/roughness.jpg')
-
-  textures.textureGradient = textureLoader.load('/textures/gradients/5.jpg')
-  textures.textureGradient.magFilter = THREE.NearestFilter
-
-  textures.textureMatcap = textureLoader.load('/textures/matcaps/6.png')
-
-  return textures
-
-})()
-
-/* MATERIALS */
-const material = (() => {
-
-  // Standard &/or Phisical Material
-  const material = new THREE.MeshPhysicalMaterial()
-
-        material.flatShading = false
-        material.opacity = 1
-        material.side = THREE.DoubleSide
-        material.wireframe = false
-
-        // Mapping
-        material.map = textures.textureDoorColor
-
-        material.aoMap = textures.textureDoorAmbientOcclusion
-        material.aoMapIntensity = 1
-
-        material.alphaMap = textures.textureDoorAlpha
-        material.transparent = true
-
-        material.displacementMap = textures.textureDoorHeight
-        material.displacementScale = 0.1
-
-        material.metalnessMap = textures.textureDoorMetalness
-        material.metalness = 1
-
-        material.roughnessMap = textures.textureDoorRoughness
-        material.roughness = 0
-
-        material.normalMap = textures.textureDoorNormal
-        material.normalScale.set(1,1)
-
-        // Phisical Material specific
-        material.clearcoat = 1
-        material.clearcoatRoughness = 0
-
-        material.sheen = 1
-        material.sheenColor.set(1,1,1)
-        material.sheenRoughness = 0.25
-
-        material.iridescence = 1
-        material.iridescenceIOR = 1
-        material.iridescenceThicknessRange = [ 100 , 100 ]
-
-        material.transmission = 1
-        material.ior = 1.5
-        material.thickness = 0.5
-
-  return material
-
-})()
-
-/* OBJECTS | Geometries */
-const planeGeometry = new THREE.PlaneGeometry(4,4,100,100)
-const sphereGeometry = new THREE.SphereGeometry(1.5,64,64)
-const torusGeometry = new THREE.TorusGeometry(1,0.5,100,200,Math.PI*2)
-
-/* OBJECTS | Meshes */
-const plane = (new THREE.Mesh(
-  planeGeometry,
-  material
-))
-const torus = (new THREE.Mesh(
-  torusGeometry,
-  material
-))
-const sphere = new THREE.Mesh(sphereGeometry, material)
+/* FONTS */
+const text = await createText(defaultObject.font)
 
 /* CAMERA | Init */
 const camera = (()=>{
@@ -166,7 +69,7 @@ const camera = (()=>{
   camera.position.x = 6
   camera.position.y = 6
   camera.position.z = 6
-  camera.lookAt(plane.position)
+  camera.lookAt(text.position)
   return camera
 })()
 
@@ -177,12 +80,16 @@ const controls = (()=>{
   return controls
 })()
 
-/* LIGHTS */
-const { pointLight } = toggleCustomLights(scene,true)
+/* HELPERS */
+axesHelper({ 
+  enable: debugObject.axesHelper,
+  helpers,
+  scene,
+})
 
 /* RENDERER */
 const renderer = (()=>{
-  const renderer = (new THREE.WebGLRenderer({ canvas }))
+  const renderer = new THREE.WebGLRenderer({canvas})
         renderer.setSize(sizes.width,sizes.height)
         renderer.setPixelRatio(Math.min(window.devicePixelRatio,2))
   return renderer
@@ -232,222 +139,32 @@ const gui = (() => {
 
           folder
             .add(debugObject,'axesHelper')
-            .onChange(val => axesHelper(scene,val))
+            .onChange(enable => axesHelper({enable,helpers,scene}))
             .name('Axis Helper')
 
           folder
             .addColor(debugObject,'color')
-            .onChange(() => material.color.set(debugObject.color))
+            .onChange(() => text.material.color.set(debugObject.color))
 
           folder
-            .add(defaultObject,'rotationSpeed')
-            .min(0)
-            .max(5)
-            .step(0.1)
-            .name('Rotation Speed')
+            .add(debugObject,'rotationEnabled')
+            .onChange(enable => enable ? rotationSpeedController.show() : rotationSpeedController.hide())
+
+          const rotationSpeedController = (
+            folder
+              .add(defaultObject,'rotationSpeed')
+              .min(0)
+              .max(5)
+              .step(0.1)
+              .name('Rotation Speed')
+              .hide()
+          )
+
+          if (debugObject.rotationEnabled) rotationSpeedController.show()
 
           folder
-            .add(material,'wireframe')
+            .add(text.material,'wireframe')
             .name('Wireframe')
-
-  })()
-
-  // Point Light
-  const customLights = (()=>{
-
-    const folder = gui.addFolder('Custom Lights').close()
-
-    const pointLightDistance = (
-      folder
-        .add(pointLight.position, 'z')
-        .min(1)
-        .max(10)
-        .step(0.1)
-        .onFinishChange((val) => pointLight.position.setComponent(2, val))
-        .name('Point Light Distance')
-        .hide()
-    )
-
-    if (debugObject.customLights) pointLightDistance.show()
-
-    folder
-      .add(debugObject,'customLights')
-      .onChange(enable => {
-        toggleCustomLights(scene,enable)
-        if (enable) pointLightDistance.show()
-        else pointLightDistance.hide()
-      })
-      .name('Active')
-
-  })()
-
-  // Phong Material
-  /*const phongFolder = (()=>{
-
-    const folder = gui.addFolder('Phong Material')
-
-          folder
-            .add(material,'shininess')
-            .min(0)
-            .max(200)
-            .step(1)
-            .name('Shininess')
-
-          folder
-            .addColor(material,'specular')
-            .name('Specular')
-
-  })()*/
-
-  // Standard Material Folder
-  /* const standardFolder = (() => {
-
-    const folder = gui.addFolder('Material | Standard')
-
-          // Flags
-          folder
-            .add(material,'flatShading')
-            .name('Flat Shading')
-
-          folder
-            .add(material,'opacity')
-            .min(0)
-            .max(1)
-            .step(0.1)
-            .name('Opacity')
-
-          // Ranges
-          folder
-            .add(material,'aoMapIntensity')
-            .min(0)
-            .max(1)
-            .step(0.1)
-            .name('Ambient Occlusion Map Intensity')
-      
-          folder
-            .add(material,'displacementScale')
-            .min(0)
-            .max(2)
-            .step(0.1)
-            .name('Displacement Scale')
-      
-          folder
-            .add(material.normalScale, 'x')
-            .min(0)
-            .max(1)
-            .step(0.001)
-            .onChange((val) => material.normalScale.setComponent(0, val))
-            .name('Normal Scale X')
-
-          folder
-            .add(material,'metalness')
-            .min(0)
-            .max(1)
-            .step(0.0001)
-            .name('Metalness')
-
-          folder
-            .add(material.normalScale, 'y')
-            .min(0)
-            .max(1)
-            .step(0.001)
-            .onChange((val) => material.normalScale.setComponent(1, val))
-            .name('Normal Scale Y')
-
-          folder
-            .add(material,'roughness')
-            .min(0)
-            .max(1)
-            .step(0.0001)
-            .name('Roughness')
-
-  })()*/
-
-  // Phisical Material Folder
-  const phisicalFolder = (() => {
-
-    const folder = gui.addFolder('Material | Phisical')
-
-          folder
-            .add(material,'clearcoat')
-            .min(0)
-            .max(1)
-            .step(0.0001)
-            .name('Clearcoat')
-        
-          folder
-            .add(material,'clearcoatRoughness')
-            .min(0)
-            .max(1)
-            .step(0.0001)
-            .name('Clearcoat Roughness')
-
-          folder
-            .add(material,'iridescence')
-            .min(0)
-            .max(1)
-            .step(0.0001)
-            .name('Iridescence')
-        
-          folder
-            .addColor(material, 'iridescenceIOR')
-            .min(1)
-            .max(2.333)
-            .step(0.0001)
-            .name('Iridescence IOR')
-        
-          folder
-            .add(material.iridescenceThicknessRange,'0')
-            .min(1)
-            .max(1000)
-            .step(1)
-            .name('Iridescence Thickness Range 0')
-        
-          folder
-            .add(material.iridescenceThicknessRange,'1')
-            .min(1)
-            .max(1000)
-            .step(1)
-            .name('Iridescence Thickness Range 1')
-
-          folder
-            .add(material,'sheen')
-            .min(0)
-            .max(1)
-            .step(0.0001)
-            .name('Sheen')
-        
-          folder
-            .addColor(material,'sheenColor')
-            .name('Sheen Color')
-        
-          folder
-            .add(material,'sheenRoughness')
-            .min(0)
-            .max(1)
-            .step(0.0001)
-            .name('Sheen Roughness')
-
-          folder
-            .add(material,'transmission')
-            .min(0)
-            .max(1)
-            .step(0.0001)
-            .name('Transmission')
-
-          folder
-            .add(material,'ior')
-            .min(1)
-            .max(10)
-            .step(0.0001)
-            .name('Index Of Reflection')
-
-          folder
-            .add(material,'thickness')
-            .min(0)
-            .max(1)
-            .step(0.0001)
-            .name('Index Of Reflection')
 
   })()
 
@@ -455,15 +172,9 @@ const gui = (() => {
 
 })()
 
-/* INIT | placements */
-torus.position.x = 4
-sphere.position.x = -4
-
 /* SCENE */
 scene.add(camera)
-scene.add(plane)
-scene.add(sphere)
-scene.add(torus)
+scene.add(text)
 
 /* ANIMATE */
 function animate() {
@@ -471,14 +182,15 @@ function animate() {
   const elapsedTime = clock.getElapsedTime()
 
   rotation({
+    active: debugObject.rotationEnabled,
     elapsedTime,
-    meshes: [ plane , sphere , torus ],
+    meshes: [ text ],
     speed: defaultObject.rotationSpeed,
   })
 
   controls.update()
 
-  renderer.render(scene, camera)
+  renderer.render(scene,camera)
   window.requestAnimationFrame(animate)
 
 } animate()
